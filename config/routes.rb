31867c39f8aec2b63947f3b262375572b1fa728f
youngsuklee <youngl@princeton.edu>
2011-11-05 01:15:37 -0400

    layout and routing update

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 AnnotateArray::Application.routes.draw do
+  match '/contact', :to => 'pages#contact'
+  match '/about', :to => 'pages#about'
+  #match '/help', :to => 'pages#help'
+  #match '/', :to => 'pages#home'
+
+  get "pages/home"
+
   resources :annotations
 
   resources :users
@@ -52,7 +59,7 @@ AnnotateArray::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => "welcome#index"
+  root :to => "pages#home"
 
   # See how all your routes lay out with "rake routes"
 
